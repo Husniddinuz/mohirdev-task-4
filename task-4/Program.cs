248bc0cdd9868cb720b5f390f8e2953134c03198
Husniddin Tugizov <husniddindev@gmail.com>
2024-05-16 00:10:20 +0500

    Mohirdev task

---
+++ task-4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Birinchi raqamni kiriting:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("Operatsiyani kiriting (+, -, *, /):");
+        char operation = Console.ReadLine()[0];
+        
+        Console.WriteLine("Ikkinchi raqamni kiriting:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        
+        double result = 0;
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Noto'g'ri operatsiya!");
+                return;
+        }
+        
+        Console.WriteLine($"Natija: {result}");
+    }
+}
